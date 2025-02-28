718a436913987dfc14857ebd88b04116844088b2
yanrbts <772166784@qq.com>
2025-02-28 18:33:19 +0800

    init

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/yrb/src/container
-# Build directory: /home/yrb/src/container/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
--- build/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/yrb/src/container/src
-# Build directory: /home/yrb/src/container/build/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
